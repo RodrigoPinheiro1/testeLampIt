0f6ee2b3f0a8a7a9bee70873dfa92083b59d8e43
Rodrigo Pinheiro Silva <45176434+RodrigoPinheiro1@users.noreply.github.com>
2023-07-12 14:56:04 -0300

    movidos metodos para PedidoController

--- src/test/java/br/com/lamppit/teste/controller/ClienteControllerTest.java
+++ src/test/java/br/com/lamppit/teste/controller/ClienteControllerTest.java
@@ -2,15 +2,12 @@ package br.com.lamppit.teste.controller;
 
 import br.com.lamppit.teste.dto.ClienteDto;
 import br.com.lamppit.teste.dto.EnderecoDto;
-import br.com.lamppit.teste.dto.LoginForm;
+import br.com.lamppit.teste.exceptions.NotFound.CepNotFoundException;
 import br.com.lamppit.teste.model.Cliente;
 import br.com.lamppit.teste.model.Endereco;
-import br.com.lamppit.teste.model.Usuario;
 import br.com.lamppit.teste.repository.ClienteRepository;
-import br.com.lamppit.teste.repository.UsuarioRepository;
-import br.com.lamppit.teste.security.TokenService;
+import br.com.lamppit.teste.service.impl.CepService;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
@@ -20,17 +17,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import springfox.documentation.spring.web.json.Json;
 
 import java.net.URI;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -43,30 +37,24 @@ class ClienteControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+
     private final URI uri = URI.create("/cliente");
     @Mock
     private ClienteDto clienteDto;
-
-    @MockBean
-    private TokenService tokenService;
-    @Autowired
-    private UsuarioRepository usuarioRepository;
-
-    @MockBean
-    private LoginForm form;
     @MockBean
     private Cliente cliente;
 
     @MockBean
     private Endereco endereco;
 
-    private String token;
+    @Autowired
+    private CepService cepService;
+
     @Autowired
     private ClienteRepository clienteRepository;
     @MockBean
     private EnderecoDto enderecoDto;
-    @MockBean
-    private JSONObject jsonObject;
+
 
     @BeforeEach
     void setUp(){
@@ -74,15 +62,15 @@ class ClienteControllerTest {
         cliente = new Cliente(endereco, "aaa");
         clienteRepository.save(cliente);
 
-
-
         enderecoDto = new EnderecoDto("08568000", "2393", "aa");
         clienteDto = new ClienteDto(enderecoDto, "aaa");
+
+
 
     }
 
     @Test
-    void cadastrarCliente() throws Exception {
+    void shouldInsertClientPassingCorrectsInformations() throws Exception {
 
         mockMvc.perform(MockMvcRequestBuilders.post(uri).
                         contentType(MediaType.APPLICATION_JSON)
@@ -91,8 +79,36 @@ class ClienteControllerTest {
 
 
     }
+    @Test
+    void shouldNotInsertClientPassingWrongInformations() throws Exception {
+
+        clienteDto.setEndereco(null);
+
+        mockMvc.perform(MockMvcRequestBuilders.post(uri).
+                        contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(clienteDto)))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+
+
+    }
 
     @Test
-    void pedidosNaoEntregues() {
+    void shouldReturnCepNotFoundPassingCepWrong() throws Exception {
+
+        enderecoDto.setCep("8u213-9i213-3r");
+
+        mockMvc.perform(MockMvcRequestBuilders.post(uri).
+                        contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(clienteDto)))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+
+        assertThrows(CepNotFoundException.class,() -> cepService.viaCep(enderecoDto));
+
     }
+
+
+
+
+
+
 }
