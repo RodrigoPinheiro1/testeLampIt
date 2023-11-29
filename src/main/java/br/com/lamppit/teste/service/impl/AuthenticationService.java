916b5988941a92c0eacb8466b02d62d03958472a
RodrigoPinheiro1 <rodrigopsps2015@gmail.com>
2023-11-29 11:47:03 -0300

    refatorado codigo da aplicacao

--- src/main/java/br/com/lamppit/teste/service/impl/AuthenticationService.java
+++ src/main/java/br/com/lamppit/teste/service/impl/AuthenticationService.java
@@ -1,39 +1,28 @@
 package br.com.lamppit.teste.service.impl;
 
 import br.com.lamppit.teste.dto.LoginForm;
-import br.com.lamppit.teste.dto.TokenDto;
 import br.com.lamppit.teste.model.Perfil;
 import br.com.lamppit.teste.model.Usuario;
 import br.com.lamppit.teste.repository.PerfilRepository;
-import br.com.lamppit.teste.repository.UsuarioRepository;
 import br.com.lamppit.teste.security.TokenService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class AuthenticationService  {
+@RequiredArgsConstructor
+public class AuthenticationService {
 
 
-    @Autowired
-    private TokenService tokenService;
+    private final TokenService tokenService;
 
-
-    @Autowired
-    private PerfilRepository perfilRepository;
-    @Autowired
-    private AuthenticationManager authenticationManager;
+    private final PerfilRepository perfilRepository;
+    private final AuthenticationManager authenticationManager;
 
     public String autenticar(LoginForm form) {
-
 
 
         var authenticationToken = new UsernamePasswordAuthenticationToken(form.getEmail(), form.getSenha());
